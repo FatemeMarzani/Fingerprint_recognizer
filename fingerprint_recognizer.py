b045184d9937551f573c0044da7b65b91e3f391c
fateme marzani <f.marzani.ut@gmail.com>
2021-03-08 13:21:25 +0330

    Add sequence recognizer

--- fingerprint_recognizer.py
+++ fingerprint_recognizer.py
@@ -1,27 +1,18 @@
-from typing import Iterable
-from libs.binary_reader import BinaryReader
-from libs.pcap_reader import PcapReader
-
 
 class FingerprintRecognizer(object):
     """
     """
 
-    def __init__(self, flows: dict, symbols: dict):
-        self.flows = flows
+    def __init__(self, symbols: dict, config: dict = {}):
+        self.config = {}
         self.symbols = symbols
-        self.symbol_sequence: dict
-    
-    def recognize(self):
+        self.config.update(self.default_config)
+        self.config.update(config)
 
-        # 1: Split flows into batches
-        
-        # 2: Generate cluster of destinations
+    
+    def recognize(self, flows):
         pass
 
     def get_symbols(self) -> dict:
         return self.symbols
-
-    def get_symbol_sequences_per_app(self) -> dict:
-        return self.symbol_sequences_per_app;
 
